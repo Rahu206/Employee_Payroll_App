7bb4c71e5c77e40075149888fe7127c6fbcedeb4
Rahu206 <rahuldeshpande93@gmail.com>
2021-08-31 23:24:07 +0530

    added: function to reset the values from the form

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -55,11 +55,11 @@ const createEmployeePayroll = () => {
 }
 const getSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
-    let selectedItems = [];
+    let setdItems = [];
     allItems.forEach(item => {
         if (item.checked) selectedItems.push(item.value);
     });
-    return selectedItems;
+    return setItems;
 }
 /*
  *1: querySelector is newer feature.
@@ -83,3 +83,31 @@ const getInputElementValue = (id) => {
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
 }
+//UC13 Reset the values of the page
+const resetForm = () => {
+    setValue('#name', ' ');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', ' ');
+    setValue('#notes', ' ');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+} 
